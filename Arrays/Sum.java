d6c79a5313bec0b5074abe8cbd9eb5ff970d90db
md0011 <mayurd0303@gmail.com>
2024-02-25 23:00:07 +0530

    Sum of all elements

---
+++ Arrays/Sum.java
@@ -0,0 +1,31 @@
+/*
+Sample Input
+10  20 30 40 50
+
+Output
+
+Sum of all elements = 150
+*/
+
+import java.util.Scanner;
+
+class Sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, i, sum = 0;
+
+        System.out.print("Enter no. of elements you want in array: ");
+        n = in.nextInt(); // enter no. of elements
+        int[] array = new int[n]; // allocating memory to array
+
+        System.out.println("Enter the elements for addition: ");
+        // scan elements of array
+        for (i = 0; i < n; i++)
+            array[i] = in.nextInt();
+
+        for (i = 0; i < n; i++) {
+            sum = sum + array[i];
+        }
+        System.out.println("Sum of all elements = " + sum);
+    }
+}
