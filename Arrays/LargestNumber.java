7f13202108f60730f4ee79c913ff96e191463c63
md0011 <mayurd0303@gmail.com>
2024-02-25 22:46:26 +0530

    Largest number in an array

---
+++ Arrays/LargestNumber.java
@@ -0,0 +1,42 @@
+/*
+Sample input
+Enter no. of elements you want in array:5
+Enter all the elements:
+2
+3
+3
+4
+3
+
+Sample output
+Largest Number in an array: 4
+*/
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    static int find(int[] a, int n) {
+        int max = a[0];
+        for (int i = 0; i < n; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n, i;
+        System.out.print("Enter no. of elements you want in array: ");
+        n = in.nextInt(); // enter no. of elements
+        int[] array = new int[n]; // allocating memory to array
+
+        // scan elements of array
+        System.out.println("Enter all the elements:");
+        for (i = 0; i < n; i++)
+            array[i] = in.nextInt();
+
+        System.out.println("Largest Number in an array: " + find(array, n));
+    }
+}
